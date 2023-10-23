16b7164818d3c5647242a2588773ce3f85ea6755
sjlee <happybin2013@google.com>
2023-10-24 03:32:57 +0900

    end: python 061~090

--- Python_Study/python_031-060.py
+++ Python_Study/python_031-060.py
@@ -5,137 +5,137 @@
 #031 아래 코드의 실행 결과를 예상해보세요.
 a = "3"
 b = "4"
-print(a + b) #34
+print("#031: ",a + b) #34
 
 #032 아래 코드의 실행 결과를 예상해보세요.
-print("Hi" * 3) #HiHiHi
+print("#032: ","Hi" * 3) #HiHiHi
 
 #033 화면에 '-'를 80개 출력하세요.
-print("-" * 80)
+print("#033: ","-" * 80)
 
 #034 변수에 다음과 같은 문자열이 바인딩되어 있습니다.
 t1 = 'python'
 t2 = 'java'
 t3 = t1 + " " + t2 + " "
-print(t3 * 4)
+print("#034: ",t3 * 4)
 
 #35 변수에 다음과 같이 문자열과 정수가 바인딩되어 있을 때 % formatting을 사용해서 다음과 같이 출력해보세요.
 name1 = "김민수"
 age1 = 10
 name2 = "이철희"
 age2 = 13
-print("이름: %s 나이: %d" %(name1,age1))
-print("이름: %s 나이: %d" %(name2,age2))
+print("#035: ","이름: %s 나이: %d" %(name1,age1))
+print("#035: ","이름: %s 나이: %d" %(name2,age2))
 
 #036 문자열의 format( ) 메서드를 사용해서 035번 문제를 다시 풀어보세요.
-print("이름: {} 나이: {}".format(name1,age1))
-print("이름: {} 나이: {}".format(name2,age2))
+print("#036: ","이름: {} 나이: {}".format(name1,age1))
+print("#036: ","이름: {} 나이: {}".format(name2,age2))
 
 #037 파이썬 3.6부터 지원하는 f-string을 사용해서 035번 문제를 다시 풀어보세요.
-print(f"이름: {name1} 나이: {age1}")
-print(f"이름: {name2} 나이: {age2}")
+print("#037: ",f"이름: {name1} 나이: {age1}")
+print("#037: ",f"이름: {name2} 나이: {age2}")
 
 #038 삼성전자의 상장주식수가 다음과 같습니다. 컴마를 제거한 후 이를 정수 타입으로 변환해보세요.
 상장주식수 = "5,969,782,550"
-print(int(상장주식수.replace(",","")))
+print("#038: ",int(상장주식수.replace(",","")))
 
 #039 다음과 같은 문자열에서 '2020/03'만 출력하세요.
 분기 = "2020/03(E) (IFRS연결)"
-print(분기[:7])
+print("#039: ",분기[:7])
 
 #040 문자열의 좌우의 공백이 있을 때 이를 제거해보세요.
 data = "   삼성전자    "
-print(data.strip())
+print("#040: ",data.strip())
 
 #041 다음과 같은 문자열이 있을 때 이를 대문자 BTC_KRW로 변경하세요.
 ticker = "btc_krw"
-print(ticker.upper())
+print("#041: ",ticker.upper())
 
 #042 다음과 같은 문자열이 있을 때 이를 소문자 btc_krw로 변경하세요.
 ticker = "BTC_KRW"
-print(ticker.lower())
+print("#042: ",ticker.lower())
 
 #043 문자열 'hello'가 있을 때 이를 'Hello'로 변경해보세요.
-print("hello".capitalize())
+print("#043: ","hello".capitalize())
 
 #044 파일 이름이 문자열로 저장되어 있을 때 endswith 메서드를 사용해서 파일 이름이 'xlsx'로 끝나는지 확인해보세요.
 file_name = "보고서.xlsx"
 print(file_name.endswith("xlsx"))
 if file_name.endswith("xlsx"):
-    print("xlsx으로 끝남")
-else: print("xlsx으로 끝남X")
+    print("#044: ","xlsx으로 끝남")
+else: print("#044: ","xlsx으로 끝남X")
 
 #045 파일 이름이 문자열로 저장되어 있을 때 endswith 메서드를 사용해서 파일 이름이 'xlsx' 또는 'xls'로 끝나는지 확인해보세요.
 file_name = "보고서.xlsx"
 print(file_name.endswith(("xlsx","xls")))
 if file_name.endswith(("xlsx","xls")):
-    print("xlsx 또는 xls으로 끝남")
-else: print("xlsx으로 끝남X")
+    print("#045: ","xlsx 또는 xls으로 끝남")
+else: print("#045: ","xlsx으로 끝남X")
 
 #046 파일 이름이 문자열로 저장되어 있을 때 startswith 메서드를 사용해서 파일 이름이 '2020'로 시작하는지 확인해보세요.
 file_name = "2020_보고서.xlsx"
 print(file_name.startswith("2020"))
 if file_name.startswith("2020"):
-    print("2020시작")
-else: print("2020시작X")
+    print("#046: ","2020시작")
+else: print("#046: ","2020시작X")
 
 #047 다음과 같은 문자열이 있을 때 공백을 기준으로 문자열을 나눠보세요.
 a = "hello world"
-print(a.split(" "))
+print("#047: ",a.split(" "))
 
 #048 다음과 같이 문자열이 있을 때 btc와 krw로 나눠보세요.
 ticker = "btc_krw"
-print(ticker.split("_"))
+print("#048: ",ticker.split("_"))
 
 #049 다음과 같이 날짜를 표현하는 문자열이 있을 때 연도, 월, 일로 나눠보세요.
 date = "2020-05-01"
 date_split = date.split("-")
-print("연도: " + date_split[0],"월: " +date_split[1],"일: "+ date_split[2])
+print("#049: ","연도: " + date_split[0],"월: " +date_split[1],"일: "+ date_split[2])
 
 #050 문자열의 오른쪽에 공백이 있을 때 이를 제거해보세요.
 data = "039490     "
-print(data.rstrip())
+print("#050: ",data.rstrip())
 
 #051 2016년 11월 영화 예매 순위 기준 top3는 다음과 같습니다. 영화 제목을 movie_rank 이름의 리스트에 저장해보세요. (순위 정보는 저장하지 않습니다.)
 movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
-print(movie_rank)
+print("#051: ",movie_rank)
 
 #052 051의 movie_rank 리스트에 "배트맨"을 추가하라.
 movie_rank.append("배트맨")
-print(movie_rank)
+print("#052: ",movie_rank)
 
 #053 movie_rank 리스트에는 아래와 같이 네 개의 영화 제목이 바인딩되어 있다. "슈퍼맨"을 "닥터 스트레인지"와 "스플릿" 사이에 추가하라.
 movie_rank.insert(1,"슈퍼맨")
-print(movie_rank)
+print("#053: ",movie_rank)
 
 #054 movie_rank 리스트에서 '럭키'를 삭제하라.
 del movie_rank[3]
-print(movie_rank)
+print("#054: ",movie_rank)
 
 #055 movie_rank 리스트에서 '스플릿' 과 '배트맨'을 를 삭제하라.
 del movie_rank[2]
 del movie_rank[2]
-print(movie_rank)
+print("#055: ",movie_rank)
 
 #056 lang1과 lang2 리스트가 있을 때 lang1과 lang2의 원소를 모두 갖고 있는 langs 리스트를 만들어라.
 lang1 = ["C", "C++", "JAVA"]
 lang2 = ["Python", "Go", "C#"]
 
-print(lang1 + lang2)
+print("#056: ",lang1 + lang2)
 
 #057 다음 리스트에서 최댓값과 최솟값을 출력하라. (힌트: min(), max() 함수 사용)
 nums = [1, 2, 3, 4, 5, 6, 7]
-print("max: ", max(nums))
-print("min: ", min(nums))
+print("#057: ","max: ", max(nums))
+print("#057: ","min: ", min(nums))
 
 #058 다음 리스트의 합을 출력하라.
 nums = [1, 2, 3, 4, 5]
-print(sum(nums))
+print("#058: ",sum(nums))
 
 #059 다음 리스트에 저장된 데이터의 개수를 화면에 구하하라.
 cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
-print("개수: ",len(cook))
+print("#059: ","개수: ",len(cook))
 
 #060 다음 리스트의 평균을 출력하라.
 nums = [1, 2, 3, 4, 5]
-print(sum(nums) / len(nums))
+print("#060: ",sum(nums) / len(nums))
--- Python_Study/python_061-090.py
+++ Python_Study/python_061-090.py
@@ -1,4 +1,161 @@
 # 아이펠 7기 python 기초교육
 # https://wikidocs.net/
 
-# 001~030
+# 061~090
+#061 price 변수에는 날짜와 종가 정보가 저장돼 있다. 날짜 정보를 제외하고 가격 정보만을 출력하라. (힌트 : 슬라이싱)
+price = ['20180728', 100, 130, 140, 150, 160, 170]
+print(price[1:])
+
+#062 슬라이싱을 사용해서 홀수만 출력하라.
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[::2])
+
+#063 슬라이싱을 사용해서 짝수만 출력하라.
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[1::2])
+
+#064 슬라이싱을 사용해서 리스트의 숫자를 역 방향으로 출력하라.
+nums = [1, 2, 3, 4, 5]
+print(nums[::-1])
+
+#065 interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+interest = ['삼성전자', 'LG전자', 'Naver']
+print(interest[0],interest[2])
+
+#066 interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print(" ".join(interest))
+
+#067 interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print("/".join(interest))
+
+#068 interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print("\n".join(interest))
+
+#069 회사 이름이 슬래시 ('/')로 구분되어 하나의 문자열로 저장되어 있다.
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
+
+#070 리스트에 있는 값을 오름차순으로 정렬하세요.
+data = [2, 4, 3, 1, 5, 10, 9]
+print(sorted(data))
+
+#071 my_variable 이름의 비어있는 튜플을 만들라.
+my_variable = ()
+print(my_variable)
+
+#072 2016년 11월 영화 예매 순위 기준 top3는 다음과 같다. 영화 제목을 movie_rank 이름의 튜플에 저장하라. (순위 정보는 저장하지 않는다.)
+movie_rank = ("닥터 스트레인지","스플릿","럭키")
+print(movie_rank)
+
+#073 숫자 1 이 저장된 튜플을 생성하라.
+one = (1,)
+print(one)
+
+#074 다음 코드를 실행해보고 오류가 발생하는 원인을 설명하라.
+# t = (1, 2, 3)
+# t[0] = 'a' # 튜플은 원소값 수정이 불가능
+
+#075 아래와 같이 t에는 1, 2, 3, 4 데이터가 바인딩되어 있다. t가 바인딩하는 데이터 타입은 무엇인가?
+t = 1, 2, 3, 4
+print(type(t))
+
+#076 변수 t에는 아래와 같은 값이 저장되어 있다. 변수 t가 ('A', 'b', 'c') 튜플을 가리키도록 수정 하라.
+t = ('a', 'b', 'c')
+print(t)
+t = ('A','b','c')
+print(t)
+
+#077 다음 튜플을 리스트로 변환하라.
+interest = ('삼성전자', 'LG전자', 'SK Hynix')
+interest = list(interest)
+print(type(interest))
+
+#78 다음 리스트를 튜플로 변경하라.
+interest = ['삼성전자', 'LG전자', 'SK Hynix']
+interest = tuple(interest)
+print(type(interest))
+
+#079 다음 코드의 실행 결과를 예상하라.
+temp = ('apple', 'banana', 'cake')
+a, b, c = temp
+print(a, b, c) # apple banana cake
+
+#080 1 부터 99까지의 정수 중 짝수만 저장된 튜플을 생성하라.
+data = tuple(range(2,100,2))
+print(data)
+
+'''
+기본적으로 데이터 언패킹은 좌변의 변수와 우변 데이터 개수가 같아야 합니다. 
+하지만 star expression을 사용하면 변수의 개수가 달라도 데이터 언패킹을 할 수 있습니다. 
+튜플에 저장된 데이터 중에서 앞에 있는 두 개의 데이터만 필요할 경우 나머지 데이터의 언패킹 코드를 작성할 필요가 없습니다.
+'''
+#081 다음과 같이 10개의 값이 저장된 scores 리스트가 있을 때, start expression을 사용하여 좌측 8개의 값을 valid_score 변수에 바인딩하여라.
+scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+*valid_score, a, b =  scores
+print(*valid_score)
+
+#082 다음과 같이 10개의 값이 저장된 scores 리스트가 있을 때, start expression을 사용하여 우측 8개의 값을 valid_score 변수에 바인딩하여라.
+scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+a, b, *valid_score =  scores
+print(*valid_score)
+
+#083 다음과 같이 10개의 값이 저장된 scores 리스트가 있을 때, start expression을 사용하여 가운데 있는 8개의 값을 valid_score 변수에 바인딩하여라.
+scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+a, *valid_score, b =  scores
+print(*valid_score)
+
+#084 temp 이름의 비어있는 딕셔너리를 만들라.
+temp = {}
+print(temp)
+
+#085 다음 아이스크림 이름과 희망 가격을 딕셔너리로 구성하라.
+price = {"메로나":1000,"폴라포":1200,"빵빠레":1800}
+print(price)
+
+#086 085 번의 딕셔너리에 아래 아이스크림 가격정보를 추가하라.
+price["죠스바"] = 1200
+price["월드콘"] = 1500
+print(price)
+
+#087 다음 딕셔너리를 사용하여 메로나 가격을 출력하라.
+ice = {'메로나': 1000,
+       '폴로포': 1200,
+       '빵빠레': 1800,
+       '죠스바': 1200,
+       '월드콘': 1500}
+
+print("메로나 가격: ", ice["메로나"])
+
+#088 다음 딕셔너리에서 메로나의 가격을 1300으로 수정하라.
+ice = {'메로나': 1000,
+       '폴로포': 1200,
+       '빵빠레': 1800,
+       '죠스바': 1200,
+       '월드콘': 1500}
+ice["메로나"] = 1300
+print(ice["메로나"])
+
+#089 다음 딕셔너리에서 메로나를 삭제하라.
+ice = {'메로나': 1000,
+       '폴로포': 1200,
+       '빵빠레': 1800,
+       '죠스바': 1200,
+       '월드콘': 1500}
+
+del ice["메로나"]
+print(ice)
+
+#090 다음 코드에서 에러가 발생한 원인을 설명하라.
+'''
+>> icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+>> icecream['누가바']
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    icecream['누가바']
+KeyError: '누가바'
+'''
+# key값이 없기 때문에 발생
